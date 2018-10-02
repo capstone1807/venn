c8071224f3f0d140a9c18560dae28dca9cb1eb73
Laura R <laurarachel@gmail.com>
2018-10-02 11:26:21 -0700

    feat(events): create event model, describe associations between events and users

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,7 +1,15 @@
 const User = require('./user')
+const Event = require('./event')
 
-User.belongsToMany(User, {as: 'Friends', through: 'friendship'})
+User.belongsToMany(User, {as: 'friends', through: 'friendship'})
+
+Event.belongsToMany(User, {through: 'event_guest'})
+User.belongsToMany(Event, {as: 'guests', through: 'event_guest'})
+
+Event.belongsTo(User, {as: 'creator'})
+User.hasMany(Event, {foreignKey: 'creatorId'})
 
 module.exports = {
-  User
+  User,
+  Event
 }
