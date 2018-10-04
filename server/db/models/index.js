7e3d4e5d8116e7109d8ad35058c1da3e189e7995
Gittie Atlas <gittieatlas@gmail.com>
2018-10-04 14:51:33 -0400

    chore: remove comments

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -5,9 +5,6 @@ const Restaurant = require('./restaurants')
 User.belongsToMany(User, {as: 'friends', through: 'friendship'})
 
 User.belongsToMany(Restaurant, {through: 'fav_restaurants'})
-// what we want
-// User.hasMany(Restaurant, {through: 'fav_restaurants'})
-// Restaurant.belongsToMany(User)
 
 Event.belongsToMany(User, {through: 'event_guest'})
 User.belongsToMany(Event, {as: 'guests', through: 'event_guest'})
