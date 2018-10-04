172749908d87e1310bae96727c632cc0b3c09ce1
Gittie Atlas <gittieatlas@gmail.com>
2018-10-04 15:41:40 -0400

    suggestions from Omri

--- server/db/models/event-restaurants.js
+++ server/db/models/event-restaurants.js
@@ -2,26 +2,18 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const EventRestaurants = db.define('event_restaurants', {
-  eventId: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  restaurantId: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
   score: {
     type: Sequelize.DECIMAL(10, 2),
     allowNull: false
   },
-  final:{
+  isFinal: {
     type: Sequelize.BOOLEAN,
     defaultValue: false
   }
 })
 
-EventRestaurants.prototype.updateScore = function (importance){
-  this.score =  parseFloat(this.score) + parseFloat(importance)
+EventRestaurants.prototype.updateScore = function(importance) {
+  this.score = parseFloat(this.score) + parseFloat(importance)
   return this.score
 }
 
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,22 +1,26 @@
 const User = require('./user')
 const Event = require('./event')
 const Restaurant = require('./restaurants')
-const EventRestaurants = require('./event-restaurants')
+const EventRestaurant = require('./event-restaurants')
 
 User.belongsToMany(User, {as: 'friends', through: 'friendship'})
 
 User.belongsToMany(Restaurant, {through: 'fav_restaurants'})
 
-Event.belongsToMany(User, {through: 'event_guest'})
-User.belongsToMany(Event, {as: 'guests', through: 'event_guest'})
+Event.belongsToMany(User, {through: 'event_user'}) // -> model object
+User.belongsToMany(Event, {through: 'event_user'})
 
-Event.belongsTo(User, {as: 'creator'})
-// User.hasMany(Event, {foreignKey: 'creatorId'})
-// ^ do we need this?
+// Event.belongsTo(User, {as: 'creator'})
+// isAdmin
+
+// Event.belongsToMany(Restaurant, {through: EventRestaurant})
+EventRestaurant.belongsTo(User)
+EventRestaurant.belongsTo(Event)
+EventRestaurant.belongsTo(Restaurant)
 
 module.exports = {
   User,
   Event,
   Restaurant,
-  EventRestaurants
+  EventRestaurant
 }
