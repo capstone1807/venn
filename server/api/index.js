b484e7ed7244aa6931e9854716e9e8a89eb202d9
Gittie Atlas <gittieatlas@gmail.com>
2018-10-05 00:24:47 -0400

    feat(events list): me/events api

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/events', require('./events'))
 router.use('/restaurants', require('./restaurants'))
+router.use('/me', require('./me'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
