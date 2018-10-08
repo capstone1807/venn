dfcd8bf274ae8f335e6a0a7c1a6907daa87af4f7
Tess Wolterstorff <tesswolterstorff@gmail.com>
2018-10-07 21:49:06 -0500

    feat(sidebar): sidebar set up but links not pushing

---
+++ client/components/Navbar/Sidebar.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {logout} from '../../store'
+import App from '../../app'
+import { Button, Icon, Menu, Segment, Sidebar } from 'semantic-ui-react'
+
+class SidebarMenu extends Component {
+  state = { visible: false }
+
+  handleButtonClick = () => this.setState ((prevState) => ({ visible: !prevState.visible }))
+
+  handleSidebarHide = () => this.setState({ visible: false })
+
+  render() {
+    const { visible } = this.state
+    const { isLoggedIn, handleClick } = this.props
+
+    return (
+      <div>
+        <Button onClick={this.handleButtonClick}><Icon name='bars'/></Button>
+
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            icon='labeled'
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={visible}
+            width='thin'
+          >
+          <Menu.Item>
+              <Link to="/events"> <Icon name='calendar alternate' /></Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to="/friends"> <Icon name='users' /></Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to="/restaurants"> <Icon name='food' /></Link>
+            </Menu.Item>
+            {isLoggedIn &&
+              <Menu.Item>
+              <Link to="/login" onClick={handleClick}> <Icon name='log out' />log out</Link>
+            </Menu.Item>}
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Segment basic>
+              <App />
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
+    )
+  }
+}
+
+const mapState = state => {
+  return {
+    isLoggedIn: !!state.user.id
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    handleClick() {
+      dispatch(logout())
+    }
+  }
+}
+
+export default connect(mapState, mapDispatch)(SidebarMenu)
+
+/**
+ * PROP TYPES
+ */
+SidebarMenu.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired
+}
--- client/index.js
+++ client/index.js
@@ -4,12 +4,12 @@ import {Provider} from 'react-redux'
 import {Router} from 'react-router-dom'
 import history from './history'
 import store from './store'
-import App from './app'
+import Sidebar from './components/Navbar/Sidebar'
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
-      <App />
+      <Sidebar />
     </Router>
   </Provider>,
   document.getElementById('app')
